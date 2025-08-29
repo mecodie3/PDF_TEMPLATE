383253c7da098f2fa06e29f022058423e5b5f673
redroze <mecodie3@gmail.com>
2025-08-30 00:09:19 +0100

    Initial Commit from CSV to PDF

---
+++ main.py
@@ -0,0 +1,22 @@
+#create pdf instances
+from fpdf import FPDF
+#data analysis
+import pandas as pd
+
+pdf = FPDF(orientation="P",unit="mm",format="A4")
+#create dataframe
+df=pd.read_csv("topics.csv")
+
+for index,row in df.iterrows():
+
+    #add pages to pdf the object instance
+    pdf.add_page()
+    pdf.set_font(family="Times" , style="B", size=24)
+    #grey color
+    pdf.set_text_color(100,100,100)
+    pdf.cell(w=0,h=12,txt=row["Topic"],align="L",
+         ln=1)
+    pdf.line(10,21,200,21)
+pdf.output("output.pdf")
+
+
